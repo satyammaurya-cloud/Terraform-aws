b29325fc54d1fb940c5da9e9de2b6ad4938b6652
satyam <satyammaurya1001@gmail.com>
2025-12-30 15:02:02 +0530

    Day-1

--- Day-1/variables.tf
+++ Day-1/variables.tf
@@ -1,13 +1,13 @@
 variable "ami_id" {
     description = "passing the variables value to main.tf"
     type = string
-    default = "ami-068c0051b15cdb816"
+    default = ""
 
 }
 
 variable "ec2_family" {
     description = "instance family"
     type = string
-    default = "t2.micro"
+    default = ""
   
 }
