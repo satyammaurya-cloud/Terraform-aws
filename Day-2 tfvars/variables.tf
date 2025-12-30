9a2b6514e418f3669c01b35bb470e202d1f30144
satyam <satyammaurya1001@gmail.com>
2025-12-30 23:14:40 +0530

    Day-2 tfvars folder added

---
+++ Day-2 tfvars/variables.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+    description = "call value into my main.tf dile"
+    type = string
+    default = ""
+}
+
+variable "ec2_family" {
+    type = string
+    default = ""
+  
+}
